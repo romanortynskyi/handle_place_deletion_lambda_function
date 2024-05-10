d38503924092abbe4af2cf799724daa6bb9264dc
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2024-05-10 12:44:10 +0300

    Implemented handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,34 @@
+import os
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+import boto3
+
 def lambda_handler(event, context):
-  print('hello')
+  place_id = event['place']['id']
+
+  host = os.environ['OPENSEACH_ENDPOINT']
+  region = os.environ['REGION']
+  service = 'es'
+  credentials = boto3.Session().get_credentials()
+  auth = AWSV4SignerAuth(credentials, region, service)
+
+  open_search_client = OpenSearch(
+      hosts = [{ 'host': host, 'port': 443 }],
+      http_auth = auth,
+      use_ssl = True,
+      verify_certs = True,
+      connection_class = RequestsHttpConnection,
+      pool_maxsize = 20
+  )
+
+  open_search_client.delete_by_query(
+    index = 'events',
+    body = {
+      'query': {
+        'match': {
+          'placeId': place_id,
+        },
+      },
+    },
+  )
+
+  return { 'message': f'Deleted events for place {place_id}' }
